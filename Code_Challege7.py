244e7878d75ab3b077c3d355efc30b09cdf65851
Satin-Emmanuel <satinemmanueljoseeee@gmail.com>
2024-12-18 02:19:34 +0800

    Yes commit

--- Code_Challege7.py
+++ Code_Challege7.py
@@ -1,66 +1,35 @@
-A = input("DID YOU BUY A MEAT GOOD/s (yes/no)? ")
-if A.upper() == "YES":
-	print('\nTHIS ARE THE LIST OF AVAILABLE MEAT GOODS')
-	print('===========================================')
-	print('Pork ------- 300php/kilo')
-	print('Chicken ---- 250php/kilo')
-	print('Beef ------- 400php/kilo')
-	print('Rabbit ----- 250php/kilo')
-	print('Tocino ----- 100php/kilo')
-	print('Bacon ------ 120php/kilo')
-	print('Bologna -----100php/kilo')
-	quan1= eval(input("\nHow many kilos of Pork meat you want to buy? "))
-	pork=quan1 * 300
-	quan2= eval(input("\nHow many kilos of Chicken meat you want to buy? "))
-	chicken=quan2 * 250
-	quan3= eval(input("\nHow many kilos of Beef meat you want to buy? "))
-	beef=quan3 * 400
-	quan4= eval(input("\nHow many kilos of Rabbit meat you want to buy? "))
-	rabbit=quan4 * 250
-	quan5= eval(input("\nHow many kilos of Tocino meat you want to buy? "))
-	tocino=quan5 * 100
-	quan6= eval(input("\nHow many kilos of Bacon meat you want to buy? "))
-	bacon=quan6 * 120
-	quan7= eval(input("\nHow many kilos of Bologna meat you want to buy? "))
-	bologna=quan7 * 100
-	total= pork+chicken+beef+rabbit+tocino+bacon+bologna
-	tax=(pork*0.123) or (chicken*0.123) or (beef*0.123) or (rabbit*0.123) or (tocino*0.123) or (bacon*0.123) or (bologna*0.123)
-	total_and_tax= (total + tax)
-	print()
-	age = input("Are you a Senior Citizen(yes/no)? ")
-	if age.lower() =='yes':
-		disc=round(total*0.052)
-		print(f"Hi, customer, you purhased a \n{quan1}kilo of Pork meat\n{quan2}kilo of Chicken meat\n{quan3}kilo of Beef meat\n{quan4}kilo of Rabbit meat\n{quan5}kilo of Tocino meat\n{quan6}kilo of Bacon meat\n{quan7}kilo of Bologna meat\nwith the total price of {total}php plus a 12.3% tax ({tax}php) and with a discount of 5.2% ({disc}php)")
+name = input("\nEnter your name ---> ")
+grocery = input("\nDid you buy a grocery? (yes/no) ---> ")
+if grocery.lower() == "yes":
+
+	item = input("\nName of the item ---> ")
+	price = eval(input("\nPrice of the item ---> "))
+	amount = eval(input("\nExact amount given ---> "))
+	age = eval(input("\nAge ---> "))
 	
-	else:
-		disc=0
-		print(f"Hi, customer, you purhased a \n{quan1}kilo of Pork meat\n{quan2}kilo of Chicken meat\n{quan3}kilo of Beef meat\n{quan4}kilo of Rabbit meat\n{quan5}kilo of Tocino meat\n{quan6}kilo of Bacon meat\n{quan7}kilo of Bologna meat with the total price of {total}php plus a 12.3% tax(",tax,")")
-	print()
-	E = float(input("Amount Given: "))
-	print()
-	if E>= total_and_tax:
-		change = round(E - total_and_tax + disc)
-		print('=================RECEIPT===================')
-		print('Qty.    Description           Amount')
-		print(f'{quan1}x ----- PORK MEAT.........{pork}php')
-		print(f'{quan2}x ----- CHICKEN MEAT......{chicken}php')
-		print(f'{quan3}x ----- BEEF MEAT.........{beef}php ')
-		print(f'{quan4}x ----- RABBIT MEAT.......{rabbit}php')
-		print(f'{quan5}x ----- TOCINO MEAT.......{tocino}php')
-		print(f'{quan6}x ----- BACON MEAT........{bacon}php')
-		print(f'{quan7}x ----- BOLOGNA MEAT......{bologna}php')
-		print()
-		print(f'SUBTOTAL...................{total}php')
-		print(f'TAX(12.3%).................{tax}php')
-		print(f'TOTAL......................{total_and_tax}php')
-		print(f'PAY AMOUNT.................{E}php')
-		print(f'DISCOUNT(5.2%).............{disc}php') 
-		print(f'CHANGE.....................{change}php')
-		print()
-		print("==THANK YOU COME AGAIN!!==")
-	else:
-		E< total_and_tax
-		print("Insufficient Amount")
-else :
-	print("Thankyou for stopping by!")
-  
+	tax = 0.123
+	ttm = price * tax
+	total = price + ttm
+	
+	if age <= 59:
+		change = amount - total
+
+		print(f"\nHi {name} you purchased a {item} , with a price of {price} plus 12.3% of tax ({ttm}) total of ({total}) ")
+		print(f"\nAmount given ---> ({amount})php ")
+		print(f"\nChange ---> [round(change , 2)] ")
+		print("\nThank you for shopping! ")
+
+	if age >= 60:	
+		discount = 0.052
+		ddm = price * 0.052
+		dtotal = price - ddm
+		cchange = amount - dtotal
+
+		print(f"\nHi {name} you paid a price of {price}, for an {item} with a discount of 5.2% ({ddm}). The total amount you paid is (round{dtotal , 2}) ")
+		print(f"\nAmount given ---> ({amount})php ")
+		print(f"\nChange ---> {cchange} ")
+		print("\nThank you for shopping! ")
+	
+	
+else:
+	print("\nThank you for checking in")
